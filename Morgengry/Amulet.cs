f1ae89cfe01dc3faafca76e4d9ad0afe73d6a4a9
rune7536 <rune7536edu.eal.dk>
2018-10-24 11:30:25 +0200

    opgave 1

---
+++ Morgengry/Amulet.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morgengry
+{
+    public class Amulet
+    {
+        private string itemId;
+        private string design;
+        private Level quality;
+
+        public string ItemId
+        {
+            get
+            {
+                return itemId;
+            }
+            set
+            {
+                itemId = value;
+            }
+        }
+        public string Design
+        {
+            get
+            {
+                return design;
+            }
+            set
+            {
+                design = value;
+            }
+        }
+        private Level Quality
+        {
+            get
+            {
+                return quality;
+            }
+            set
+            {
+                quality = value;
+            }
+        }
+        
+
+        public enum Level
+        {
+            low, medium, high
+        }
+
+        public Amulet(string item, Level quality, string design)
+        {
+            ItemId = item;
+            Quality = quality;
+            Design = design;
+        }
+        public Amulet(string item, Level quality) :
+            this (item, quality, "Not supplied")
+        {
+            //itemId = Item;
+            //quality = Quality;
+        }
+        public Amulet(string item) :
+            this(item, Level.medium)
+        {
+            //itemId = Item;
+        }
+
+
+
+    }
+}
---
+++ Morgengry/Book.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morgengry
+{
+    class Book
+    {
+        private string itemId;
+        private string title;
+        private double price;
+
+        public string ItemId
+        {
+            get
+            {
+                return itemId;
+            }
+            set
+            {
+                itemId = value;
+            }
+        }
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+            }
+        }
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+            }
+        }
+
+        public Book(string item, string title, double price)
+        {
+            ItemId = item;
+            Title = title;
+            Price = price;
+        }
+        public Book(string item, string title) :
+            this (item, title, 0.0)
+        {
+            //ItemId = item;
+            //Title = title;
+        }
+        public Book(string item) :
+            this (item, "no title")
+        {
+            //ItemId = item;
+        }
+
+
+    }
+}
